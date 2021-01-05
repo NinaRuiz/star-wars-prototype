996b1d39317ba5ba175768d1ab3e68e27b4d5ea9
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-05 19:48:13 +0100

    Add input for username and password into login util and add buttons to enter and register.

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -6,13 +6,21 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { LoginUtilComponent } from './utils/login-util/login-util.component';
 import { RegisterUtilComponent } from './utils/register-util/register-util.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
   declarations: [LoginPageComponent, RegisterPageComponent, LoginUtilComponent, RegisterUtilComponent],
   imports: [
     CommonModule,
-    ProfileRoutingModule
+    ProfileRoutingModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class ProfileModule { }
