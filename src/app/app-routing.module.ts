faffa8edd9cff4c3da787590954c0d4dd9ef39a8
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-05 19:38:22 +0100

    Routed login page as a full layout children

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,12 @@ const routes: Routes = [
   {
     path: '',
     component: FullLayoutComponent,
-    children: []
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./profile/profile.module').then(m => m.ProfileModule)
+      }
+    ]
   },
   // @todo add auth guard
   {
