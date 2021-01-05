49b457b05c029ae08fd2db895ce92c50348725f8
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-05 19:26:43 +0100

    Mockup route to toolbar layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {FullLayoutComponent} from './layouts/full-layout/full-layout.component';
+import {ToolbarLayoutComponent} from './layouts/toolbar-layout/toolbar-layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: FullLayoutComponent,
+    children: []
+  },
+  // @todo add auth guard
+  {
+    path: 'toolbar-layout',
+    component: ToolbarLayoutComponent,
     children: []
   }
 ];
