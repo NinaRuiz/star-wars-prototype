7b438b536f93ffea5350280c228058f1ab6aef73
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-05 19:22:23 +0100

    Generated toolbar layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FullLayoutComponent } from './layouts/full-layout/full-layout.component';
+import { ToolbarLayoutComponent } from './layouts/toolbar-layout/toolbar-layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FullLayoutComponent
+    FullLayoutComponent,
+    ToolbarLayoutComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/layouts/toolbar-layout/toolbar-layout.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ToolbarLayoutComponent } from './toolbar-layout.component';
+
+describe('ToolbarLayoutComponent', () => {
+  let component: ToolbarLayoutComponent;
+  let fixture: ComponentFixture<ToolbarLayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ToolbarLayoutComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ToolbarLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
