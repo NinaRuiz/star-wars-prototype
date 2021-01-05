bf81b7c6eaa20c53cffa3282646f9c2d614b45c7
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-05 19:31:05 +0100

    Generated login page and register page

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProfileRoutingModule } from './profile-routing.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LoginPageComponent, RegisterPageComponent],
   imports: [
     CommonModule,
     ProfileRoutingModule
