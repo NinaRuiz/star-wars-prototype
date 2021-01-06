18ac090554e35eb47fe53c9fb6f19f0069521e26
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-06 13:45:30 +0100

    Generated starship model

---
+++ src/app/starship/models/starship-model.spec.ts
@@ -0,0 +1,7 @@
+import { StarshipModel } from './starship-model';
+
+describe('StarshipModel', () => {
+  it('should create an instance', () => {
+    expect(new StarshipModel()).toBeTruthy();
+  });
+});
