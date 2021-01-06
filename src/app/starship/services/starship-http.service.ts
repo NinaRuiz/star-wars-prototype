9d615b45cdd399e059994673044d67e133c14cf7
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-06 14:11:28 +0100

    Create Rest methods on starship http service

--- src/app/starship/services/starship-http.service.ts
+++ src/app/starship/services/starship-http.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../../../environments/environment';
+import {map} from 'rxjs/operators';
+import {StarshipModel} from '../models/starship-model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StarshipHttpService {
 
-  constructor() { }
+  private tableURL = 'starships/';
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getList() {
+  }
+
+  getById() {
+
+  }
+
+  create() {
+
+  }
+
+  update() {
+
+  }
+
+  delete() {
+
+  }
 }
