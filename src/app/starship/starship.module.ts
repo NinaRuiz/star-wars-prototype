feef1a62cd0b568c7530c15d5548da0d08c25aae
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-06 13:40:27 +0100

    Add card and starship list util component to starship list page.

--- src/app/starship/starship.module.ts
+++ src/app/starship/starship.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { StarshipRoutingModule } from './starship-routing.module';
 import { StarshipListPageComponent } from './pages/starship-list-page/starship-list-page.component';
 import { StarshipListUtilComponent } from './utils/starship-list-util/starship-list-util.component';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
   declarations: [StarshipListPageComponent, StarshipListUtilComponent],
   imports: [
     CommonModule,
-    StarshipRoutingModule
+    StarshipRoutingModule,
+    MatCardModule
   ]
 })
 export class StarshipModule { }
