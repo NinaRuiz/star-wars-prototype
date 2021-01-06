e3b3e9c47c197893edb07ddde713e0428df2b6f6
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-06 14:30:16 +0100

    Created starship model mapper and starship list model mapper.

--- src/app/starship/mappers/starship-mapper.ts
+++ src/app/starship/mappers/starship-mapper.ts
@@ -1,2 +1,47 @@
+import {StarshipModel} from '../models/starship-model';
+import {StarshipListModel} from '../models/starship-list-model';
+
 export class StarshipMapper {
+
+  static map(response): StarshipModel {
+    const model = new StarshipModel();
+
+    const keys = Object.keys(response);
+    const values = Object.values(response);
+
+    for (let i = 0; i < keys.length; i++) {
+      if (keys[i].substring(0, 1) === '_') {
+        model[keys[i]] = values[i];
+      } else {
+        model['_' + keys[i]] = values[i];
+      }
+    }
+
+    return model;
+  }
+
+  static mapList(response): StarshipListModel {
+
+    const out = [];
+
+    for (const starshipRequest of response.results) {
+      out.push(
+        this.map(starshipRequest)
+      );
+    }
+
+    const list = new StarshipListModel();
+
+    list.pageSize = 10;
+    list.pageNumber = response.next.substring(response.next.length - 1);
+    list.totalPages = response.count / list.pageSize;
+    list.totalElements = response.count;
+    list.size = 10;
+    list.numberOfElements = 10;
+
+    list.starshipList = out;
+
+    return list;
+  }
+
 }
