d34cbb32638179c8a8cc44ba714e12ab4b594eba
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-06 14:13:27 +0100

    Generated starship mapper

---
+++ src/app/starship/mappers/starship-mapper.spec.ts
@@ -0,0 +1,7 @@
+import { StarshipMapper } from './starship-mapper';
+
+describe('StarshipMapper', () => {
+  it('should create an instance', () => {
+    expect(new StarshipMapper()).toBeTruthy();
+  });
+});
