7652f000e75d44183d7e4d1cf635318fa6781046
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-10 18:22:35 +0100

    [CACHE SYSTEM] (NO TESTED) Cache method for starship list.

--- src/app/starship/services/starship-http.service.ts
+++ src/app/starship/services/starship-http.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import {map} from 'rxjs/operators';
+import {map, shareReplay} from 'rxjs/operators';
 import {StarshipModel} from '../models/starship-model';
 import {StarshipMapper} from '../mappers/starship-mapper';
 import {Observable} from 'rxjs';
@@ -12,11 +12,23 @@ import {StarshipListModel} from '../models/starship-list-model';
 })
 export class StarshipHttpService {
 
+  private cache$ = {};
+
   private tableURL = 'starships/';
 
   constructor(
     private httpClient: HttpClient
   ) {
+  }
+
+  getCacheList(pageNumber: number): Observable<StarshipListModel> {
+    if (!this.cache$[pageNumber]) {
+      this.cache$[pageNumber] = this.getList(pageNumber).pipe(
+        shareReplay(1)
+      );
+    }
+
+    return this.cache$[pageNumber];
   }
 
   getList(pageNumber: number): Observable<StarshipListModel> {
