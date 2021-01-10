25619ebe07a9369bca93e663e3c2a367cf38a943
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-10 17:34:15 +0100

    [UNIT TESTING] StarshipHttpService: Validate return type getList test

--- src/app/starship/services/starship-http.service.spec.ts
+++ src/app/starship/services/starship-http.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StarshipHttpService } from './starship-http.service';
+import {Observable} from 'rxjs';
+import {StarshipListModel} from '../models/starship-list-model';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {BrowserModule} from '@angular/platform-browser';
 
 describe('StarshipHttpService', () => {
   let service: StarshipHttpService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        StarshipHttpService
+      ]});
     service = TestBed.inject(StarshipHttpService);
   });
 
   // region good faith
+
+  it ('getListTest validate return type', () => {
+    const list: any = service.getList(1);
+    expect(list instanceof Observable).toBeTruthy();
+  });
 
   // endregion good faith
 
