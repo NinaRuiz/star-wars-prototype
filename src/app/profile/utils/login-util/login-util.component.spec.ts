d2bbc2bd3022e23705c788bcc38d5255a51db320
NinaRuiz <nina.ruizandres@gmail.com>
2021-01-11 07:58:21 +0100

    [UNIT TESTING] Import Mat Snack Bar Module and Router Testing Module into Login util and page.

--- src/app/profile/utils/login-util/login-util.component.spec.ts
+++ src/app/profile/utils/login-util/login-util.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginUtilComponent } from './login-util.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('LoginUtilComponent', () => {
   let component: LoginUtilComponent;
@@ -8,7 +10,8 @@ describe('LoginUtilComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginUtilComponent ]
+      declarations: [ LoginUtilComponent ],
+      imports: [MatSnackBarModule, RouterTestingModule]
     })
     .compileComponents();
   });
